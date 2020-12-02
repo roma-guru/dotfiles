0291fdab994cbbcc2a1aa61eef8f60d112c0d03b
Roman Voropaev <voropaev.roma@gmail.com>
2020-12-02 22:47:39 +0300

    Forgotten file

---
+++ .zsh/path.zsh
@@ -0,0 +1,23 @@
+# set PATH so it includes user's private bin if it exists
+declare -U path
+path=( . ~/.local/bin $path )
+
+# add user python scripts on Mac
+export PYTHONDEVMODE=1
+if [[ $(uname) == Darwin ]]; then
+    path=( ~/Library/Python/3.7/bin $path )
+fi
+
+# admin tools
+path=( /usr/local/sbin /usr/local/bin $path )
+
+# go path
+export GO111MODULE=on
+if [[ $(uname) == Darwin ]]; then
+    export GOPATH=~/Library/Go
+fi
+path=( $GOPATH/bin $path )
+
+# ruby for jekyll
+export PATH="/usr/local/opt/ruby/bin:$PATH"
+
