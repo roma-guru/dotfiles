b96b95ec324c1aa0a9753b084e34b3e75f83c57a
Roman Voropaev <voropaev.roma@gmail.com>
2023-11-02 23:14:14 +0200

    Doom nvim now much closer to Doom emacs

--- .doom.nv/config.lua
+++ .doom.nv/config.lua
@@ -4,30 +4,6 @@
 -- Just override stuff in the `doom` global table (it's injected into scope
 -- automatically).
 
--- ADDING A PACKAGE
---
--- doom.use_package("EdenEast/nightfox.nvim", "sainnhe/sonokai")
--- doom.use_package({
---   "ur4ltz/surround.nvim",
---   config = function()
---     require("surround").setup({mappings_style = "sandwich"})
---   end
--- })
-
--- ADDING A KEYBIND
---
--- doom.use_keybind({
---   -- The `name` field will add the keybind to whichkey
---   {"<leader>s", name = '+search', {
---     -- Bind to a vim command
---     {"g", "Telescope grep_string<CR>", name = "Grep project"},
---     -- Or to a lua function
---     {"p", function()
---       print("Not implemented yet")
---     end, name = ""}
---   }}
--- })
-
 -- ADDING A COMMAND
 --
 -- doom.use_cmd({
@@ -42,10 +18,91 @@
 -- })
 
 doom.use_package("is0n/jaq-nvim")
+doom.use_keybind({
+  {"<f5>", ":Jaq<cr>"},
+  {"-", ":e .<cr>"},
+  {"<c-s>", ":w<cr>"},
+  {"<c-right>", "<c-w>l"},
+  {"<c-left>", "<c-w>h"},
+  {"<c-up>", "<c-w>k"},
+  {"<c-down>", "<c-w>j"}
+})
+
 
 doom.indent = 2
 doom.relative_num = false
 doom.core.treesitter.settings.show_compiler_warning_message = false
 vim.opt.shell = 'xonsh'
+
+-- Quickrun config
+require('jaq-nvim').setup{
+  cmds = {
+    -- Uses vim commands
+    internal = {
+      lua = "luafile %",
+      vim = "source %"
+    },
+
+    -- Uses shell commands
+    external = {
+      python   = "python3 %",
+      go       = "go run %",
+      sh       = "bash %"
+    }
+  },
+
+  behavior = {
+    -- Default type
+    default     = "float",
+
+    -- Start in insert mode
+    startinsert = false,
+
+    -- Use `wincmd p` on startup
+    wincmd      = false,
+
+    -- Auto-save files
+    autosave    = true
+  },
+
+  ui = {
+    float = {
+      -- See ':h nvim_open_win'
+      border    = "rounded",
+
+      -- See ':h winhl'
+      winhl     = "Normal",
+      borderhl  = "FloatBorder",
+
+      -- See ':h winblend'
+      winblend  = 10,
+
+      -- Num from `0-1` for measurements
+      height    = 0.8,
+      width     = 0.8,
+      x         = 0.5,
+      y         = 0.5
+    },
+
+    terminal = {
+      -- Window position
+      position = "bot",
+
+      -- Window size
+      size     = 10,
+
+      -- Disable line numbers
+      line_no  = false
+    },
+
+    quickfix = {
+      -- Window position
+      position = "bot",
+
+      -- Window size
+      size     = 10
+    }
+  }
+}
 
 -- vim: sw=2 sts=2 ts=2 expandtab
