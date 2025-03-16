a4ffe9193052564a24a949520c3bafd3720664b3
Roman Voropaev <voropaev.roma@gmail.com>
2025-03-16 23:20:53 +0200

    major update for mac, now it's mainly for music

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -20,6 +20,7 @@ alias pf=printf
 
 alias func=function
 alias def=function
+alias cmd=command
 
 alias le='bat'
 alias la='l -a'
@@ -35,7 +36,10 @@ alias nog='noglob'
 alias loc='noglob locate'
 alias noc='nocorrect'
 alias mmv='noglob zmv -W'
-alias mkdir='mkdir -pv'
+func mkd {
+    cmd mkdir -pv $1
+    cd $1
+}
 alias mkx='chmod a+x'
 alias find='noglob find'
 alias fd='noglob fd'
@@ -49,8 +53,9 @@ alias vi="vim -u NONE"
 alias rr="r -2"
 alias rrr="r -3"
 alias killj="kill %1"
+alias md="macdown"
 
-alias vimwork='file=work && vim ~/.zsh/$file.zsh && . ~/.zsh/$file.zsh'
+#alias vimwork='file=work && vim ~/.zsh/$file.zsh && . ~/.zsh/$file.zsh'
 alias vimali='file=aliases && vim ~/.zsh/$file.zsh && . ~/.zsh/$file.zsh'
 alias vimtw='file=tweaks && vim ~/.zsh/$file.zsh && . ~/.zsh/$file.zsh'
 alias vimpath='file=path && vim ~/.zsh/$file.zsh && . ~/.zsh/$file.zsh'
@@ -61,6 +66,16 @@ alias reload='. ~/.zshrc'
 
 def del {
     mv $1 ~/.Trash
+}
+alias copy-pubkey='cat ~/.ssh/id_rsa.pub | pbcopy'
+func copy-img {
+    _ dd if=$1 of=$2 bs=4m status=progress
+}
+func copy-img.gz {
+    _ sh -c "gzcat $1 | pv > $2"
+}
+func copy-img.xz {
+    _ sh -c "xz -d $1 -c | pv > $2"
 }
 
 # Direnv
@@ -83,20 +98,19 @@ func ipinfo() {
 
 # Python
 for v in {9..12}; do
-   alias 3.$v="python3.$v"
+   alias py3.$v="python3.$v"
+   alias mkvenv3.$v="py3.$v -m venv .venv"
 done
 alias py='python3'
 alias pypy='pypy3'
 alias ipy='ipython3'
-alias mkvenv="py -m venv .venv"
 alias act=". .venv/bin/activate"
 alias deact='deactivate'
 alias pyhttp='py -m http.server 8000'
 alias pysetup='py setup.py'
-alias pyclean='rm -rf **/__pycache__'
-def pypkg() {
-    mkdir $1 && touch $1/__init__.py
-}
+alias pyclean='rm -rfv **/__pycache__ **/*.py[cod]'
+alias pypurge='pyclean && rm -rfv **/.env **/.venv **/.pytest_cache **/.mypy_cache'
+alias pypkg="touch __init__.py"
 alias pyignore='wget https://github.com/github/gitignore/raw/main/Python.gitignore -O .gitignore'
 
 alias conda-act="conda activate"
@@ -110,26 +124,45 @@ alias swc='swiftc'
 
 # Containers
 alias doc="docker"
-alias docomp="docker compose"
+alias docomp="docker-compose"
 alias pod="podman"
+func docsh {
+    doc exec -it $1 bash
+}
+alias kub="kubectl"
 
 # Pip
 alias pip=pip3
 alias pip3="py -m pip"
 alias pipi="pip3 install"
-alias pipi-test="pip3 install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple"
-alias pipi-req="pip3 install -r require*.txt"
+
+alias pipi-req="pip3 install -r req*.txt"
 alias pipi-dev="pip3 install -e ."
-alias pipi-test="pip3 install --extra-index-url https://testpypi.python.org/pypi"
+alias pipi-test="python3 -m pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/"
 alias piprm="pip3 uninstall"
 alias pipun="piprm"
 alias pipgrep="pip3 freeze | grep"
+
+# Django
+alias djr="./manage.py runserver"
+alias djm="./manage.py migrate"
+alias djmm="./manage.py makemigrations"
+alias djpasswd="./manage.py changepassword"
+alias djadmin="./manage.py createsuperuser"
 
 # Hatch
 alias ha=hatch
 alias har="hatch run"
 alias has="hatch shell"
 alias hap="hatch python"
+
+# Twine
+alias twup="twine upload"
+alias twup-test="twine -r testpypi upload"
+
+# Pipenv
+alias pesh="pipenv shell"
+alias pei="pipenv install"
 
 # Git
 alias gin="git init"
@@ -155,6 +188,7 @@ alias jupyc="jupyter console"
 hash -d desk="$HOME/Desktop"
 hash -d docs="$HOME/Documents"
 hash -d downs="$HOME/Downloads"
+hash -d music="$HOME/Music"
 
 hash -d zsh="$HOME/.zsh"
 hash -d vim="$HOME/.vim"
@@ -186,11 +220,6 @@ alias curl="nog curl"
 alias xh="nog xh"
 alias http="nog http"
 
-# transfer.sh
-def transfer() {
-    if [ $# -eq 0 ];then echo "No arguments specified.\nUsage:\n transfer <file|directory>\n ... | transfer <file_name>">&2;return 1;fi;if tty -s;then file="$1";file_name=$(basename "$file");if [ ! -e "$file" ];then echo "$file: No such file or directory">&2;return 1;fi;if [ -d "$file" ];then file_name="$file_name.zip" ,;(cd "$file"&&zip -r -q - .)|curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null,;else cat "$file"|curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null;fi;else file_name=$1;curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null;fi;
-}
-
 # dotenv helper
 def dotenv() {
     for l (`cat .env`) {
@@ -208,4 +237,26 @@ func wttr.in() {
 }
 func file.io() {
     curl -X POST -F "file=@$1" https://file.io | jq .link
+}
+
+# Brew
+alias brew-svc="brew services"
+
+# AWS vpns
+# alias start-vpn="aws ec2 start-instances --ids ..."
+# alias stop-vpn="aws ec2 stop-instances --ids ..."
+
+# Translate
+alias ru="trans ru:en"
+for lang in en el es; do
+    alias $lang="trans ${lang}:ru"
+done
+
+# Audio stuff
+func mp3_cover() {
+    if [ $# -lt 3 ]; then
+        print "Args: infile.mp3 cover.jpg outfile.mp3"
+    else
+        ffmpeg -i $1 -i $2 -map 0:0 -map 1:0 -c copy -id3v2_version 3 $3
+    fi
 }
--- .zsh/tweaks.zsh
+++ .zsh/tweaks.zsh
@@ -1,6 +1,7 @@
 # Zsh tweaks
+setopt appendhistory incappendhistory sharehistory
 setopt extendedhistory histsavenodups histfindnodups histignoredups
-setopt histexpiredupsfirst histreduceblanks sharehistory incappendhistory
+setopt histexpiredupsfirst histreduceblanks incappendhistory
 setopt autocd autopushd extendedglob chase_links noflowcontrol
 zmodload zsh/mathfunc zsh/clone zsh/stat zsh/regex
 zmodload zsh/net/tcp zsh/zftp
@@ -9,9 +10,12 @@ autoload -U zcalc
 autoload -Uz promptinit
 
 # Env
+export HISTSIZE=10000
+export SAVEHIST=10000
 export REPORTTIME=3
 export KEYTIMEOUT=0
 export DIRSTACKSIZE=10
 export EDITOR=vim
 zstyle ':completion:*' rehash true
+
 func chpwd() { k -h | head -n 10; print ... }
